3275601bc1de1660b29b5c0f2b5ee66506cfe16e
capsandiego <sergiopaternoster73@gmail.com>
2019-04-15 13:09:21 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$text = "Hello World! This is my first Alexa Skill!";
+$text = "Hello World! This is my first Alexa Skill! The forwarded is: ".$_SERVER['HTTP_X_FORWARDED_FOR']." while the IP is: ".$_SERVER['REMOTE_ADDR'] ;
 
 $array = [  "version" => "1.0",
             "response" => ["outputSpeech" => ["type" => "PlainText", "text" => $text]],
