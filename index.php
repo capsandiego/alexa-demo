c91b83571cc32e8a10322169b23f7072a6e09491
capsandiego <sergiopaternoster73@gmail.com>
2019-09-13 09:19:17 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-$text = "Hello World! This is my first Alexa Skill! The forwarded is: ".$_SERVER['HTTP_X_FORWARDED_FOR']." while the IP is: ".$_SERVER['REMOTE_ADDR'] ;
+$ip = isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
+
+$text = "Hello World! This is my first Alexa Skill! The forwarded is: ".$_SERVER['HTTP_X_FORWARDED_FOR']." while the REMOTE_ADDR IP is: ".$_SERVER['REMOTE_ADDR'] ." and IP is: ". $ip;
 
 $array = [  "version" => "1.0",
             "response" => ["outputSpeech" => ["type" => "PlainText", "text" => $text]],
